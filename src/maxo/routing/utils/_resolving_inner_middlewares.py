033548c06ae431bb0f6577934af00921fb3c44b9
K1rL3s <kirill.lesovoi@mail.ru>
2025-12-22 02:09:18 +0300

    Убрал неживые остатки костыля

--- src/maxo/routing/utils/_resolving_inner_middlewares.py
+++ src/maxo/routing/utils/_resolving_inner_middlewares.py
@@ -7,36 +7,6 @@ from maxo.routing.interfaces.router import BaseRouter
 from maxo.routing.updates.base import BaseUpdate
 
 
-def _resolving_middlewares(
-    router: BaseRouter,
-    middlewares: MutableMapping[
-        type[BaseUpdate],
-        MutableSequence[BaseMiddleware[Any]],
-    ],
-) -> None:
-    for update_tp, observer in router.observers.items():
-        new_inners = (*middlewares[update_tp],)
-        current_inners = (*observer.middleware.inner.middlewares,)
-
-        observer.middleware.inner.middlewares.extend(new_inners)
-        middlewares[update_tp].extend(current_inners)
-
-
-def _resolving_outer_middlewares(
-    router: BaseRouter,
-    middlewares: MutableMapping[
-        type[BaseUpdate],
-        MutableSequence[BaseMiddleware[Any]],
-    ],
-) -> None:
-    for update_tp, observer in router.observers.items():
-        new_middlewares = (*middlewares[update_tp],)
-        current_middlewares = (*observer.middleware.outer.middlewares,)
-
-        observer.middleware.outer.middlewares.extend(new_middlewares)
-        middlewares[update_tp].extend(current_middlewares)
-
-
 def resolve_middlewares(
     router: BaseRouter,
     middlewares: (
@@ -53,3 +23,18 @@ def resolve_middlewares(
             children_router,
             middlewares.copy(),
         )
+
+
+def _resolving_middlewares(
+    router: BaseRouter,
+    middlewares: MutableMapping[
+        type[BaseUpdate],
+        MutableSequence[BaseMiddleware[Any]],
+    ],
+) -> None:
+    for update_tp, observer in router.observers.items():
+        new_inners = (*middlewares[update_tp],)
+        current_inners = (*observer.middleware.inner.middlewares,)
+
+        observer.middleware.inner.middlewares.extend(new_inners)
+        middlewares[update_tp].extend(current_inners)
